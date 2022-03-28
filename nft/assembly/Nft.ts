a6b8893ca4a7d5a0d632e9cd226c1a1f42aa94a6
luke <luke@prescryptive.com>
2022-03-28 11:05:21 -0700

    Put approver/operator logic back

--- nft/assembly/Nft.ts
+++ nft/assembly/Nft.ts
@@ -75,6 +75,37 @@ export class Nft {
     return res;
   }
 
+  get_approved(args: nft.get_approved_arguments): nft.get_approved_result {
+    const token_id = args.token_id;
+
+    const res = new nft.get_approved_result();
+
+    const approval = this._state.GetTokenApproval(token_id);
+
+    if (approval) {
+      res.value = approval.address;
+    }
+
+    return res;
+  }
+
+  is_approved_for_all(
+    args: nft.is_approved_for_all_arguments
+  ): nft.is_approved_for_all_result {
+    const owner = args.owner as Uint8Array;
+    const operator = args.operator as Uint8Array;
+
+    const res = new nft.is_approved_for_all_result();
+
+    const approval = this._state.GetOperatorApproval(owner, operator);
+
+    if (approval) {
+      res.value = approval.approved;
+    }
+
+    return res;
+  }
+
   mint(args: nft.mint_arguments): nft.mint_result {
     const to = args.to as Uint8Array;
     const token_id = args.token_id;
@@ -181,10 +212,28 @@ export class Nft {
     const owner = token.owner as Uint8Array;
     const b58Owner = Base58.encode(owner);
 
-    if (b58Owner != b58From) {
-      System.log('transfer caller is not owner');
-      return res;
+    if (b58Owner != b58From) {  let isTokenApproved = false;
+
+      const tokenApproval = this._state.GetTokenApproval(token_id);
+
+      if (tokenApproval) {
+        const approvedAddress = tokenApproval.address as Uint8Array;
+
+        isTokenApproved = Base58.encode(approvedAddress) == b58From;
+      }
+
+      if (!isTokenApproved) {
+        const operatorApproval = this._state.GetOperatorApproval(owner, from);
+
+        if (!operatorApproval.approved) {
+          System.log('transfer caller is not owner nor approved');
+          return res;
+        }
+      }
     }
+
+    // clear the token approval
+    this._state.DeleteTokenApproval(token_id);
 
     // update the balances
     // from
@@ -206,6 +255,100 @@ export class Nft {
     const impacted = [to, from];
 
     System.event('nft.transfer', Protobuf.encode(transferEvent, nft.transfer_event.encode), impacted);
+
+    res.value = true;
+
+    return res;
+  }
+
+  approve(args: nft.approve_arguments): nft.approve_result {
+    const approver_address = args.approver_address as Uint8Array;
+    const to = args.to as Uint8Array;
+    const token_id = args.token_id;
+
+    const res = new nft.approve_result(false);
+
+    // require authority of the approver_address
+    System.requireAuthority(authority.authorization_type.contract_call, approver_address);
+
+    // check that the token exists
+    let token = this._state.GetToken(token_id);
+
+    if (!token) {
+      System.log('nonexistent token');
+      return res;
+    }
+
+    const owner = token.owner as Uint8Array;
+    const b58Owner = Base58.encode(owner);
+
+    // check that the to is not the owner
+    if (Base58.encode(to) == b58Owner) {
+      System.log('approve to current owner');
+
+      return res;
+    }
+
+    // check that the approver_address is allowed to approve the token
+    if (Base58.encode(approver_address) != b58Owner) {
+      const approval = this._state.GetOperatorApproval(owner, approver_address);
+
+      if (!approval.approved) {
+        System.log('approver_address is not owner nor approved');
+        return res;
+      }
+    }
+
+    // update approval
+    let approval = this._state.GetTokenApproval(token_id);
+
+    if (!approval) {
+      approval = new nft.token_approval_object();
+    }
+
+    approval.address = to;
+    this._state.SaveTokenApproval(token_id, approval);
+
+    // generate event
+    const approvalEvent = new nft.token_approval_event(approver_address, to, token_id);
+    const impacted = [to, approver_address];
+
+    System.event('nft.token_approval', Protobuf.encode(approvalEvent, nft.token_approval_event.encode), impacted);
+
+    res.value = true;
+
+    return res;
+  }
+
+  set_approval_for_all(
+    args: nft.set_approval_for_all_arguments
+  ): nft.set_approval_for_all_result {
+    const approver_address = args.approver_address as Uint8Array;
+    const operator_address = args.operator_address as Uint8Array;
+    const approved = args.approved;
+
+    const res = new nft.set_approval_for_all_result(false);
+
+    // only the owner of approver_address can approve an operator for his account
+    System.requireAuthority(authority.authorization_type.contract_call, approver_address);
+
+    // check that the approver_address is not the address to approve
+    if (Base58.encode(approver_address) == Base58.encode(operator_address)) {
+      System.log('approve to operator_address');
+
+      return res;
+    }
+
+    // update the approval
+    const approval = this._state.GetOperatorApproval(approver_address, operator_address);
+    approval.approved = approved;
+    this._state.SaveOperatorApproval(approver_address, operator_address, approval);
+
+    // generate event
+    const approvalEvent = new nft.operator_approval_event(approver_address, operator_address, approved);
+    const impacted = [operator_address, approver_address];
+
+    System.event('nft.operator_approval', Protobuf.encode(approvalEvent, nft.operator_approval_event.encode), impacted);
 
     res.value = true;
 
